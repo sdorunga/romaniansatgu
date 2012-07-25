bd349dff3ebc939cc8cf4bdf564aa3d602157e17
Stefan Dorunga <stefandorunga@gmail.com>
2012-07-25 14:08:36 +0300

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Romaniansatgu::Application.config.secret_token = '003c2e0d03d80b8d3d679ae734e4542063626474c7c28e33d80521a3b48c24665932ed691486ad178fcde0d1790a147eee12890d25aa8cd673fb29026464ea58'
