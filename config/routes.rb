de78416cf60d4e62466eb82276829e922e52dc6c
Stefan Dorunga <stefandorunga@gmail.com>
2012-07-25 14:29:06 +0300

    create homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Romaniansatgu::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/news"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,8 @@ Romaniansatgu::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
+  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
---
+++ test/functional/static_pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get news" do
+    get :news
+    assert_response :success
+  end
+
+end
